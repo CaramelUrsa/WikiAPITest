af2c9c998e09fce49902e5d8cf21cf914d29c136
Logan Angellotti <loganangellotti@aliasappss-iMac.local>
2019-09-27 11:10:43 -0400

    user dot map not working

--- src/index.js
+++ src/index.js
@@ -10,3 +10,58 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+class Testap extends React.Component {
+    state = {
+        isLoading: true,
+        users: [],
+        error: null
+    }
+
+    render() {
+        const { isLoading, users, error } = this.state;
+
+        return (
+            <React.Fragment>
+                <h1>Random User</h1>
+                {error ? <p>{error.message}</p> : <p>No Error</p>}
+
+                {!isLoading ? (
+                    users.map(user => {
+                        const { username, name, email } = user;
+                        return (
+                            <div key={username}>
+                                <p>Name: {name}</p>
+                                <p>Email Address: {email}</p>
+                                <hr />
+                            </div>
+                        );
+                    })
+                ) : (
+                    <h3>Loading...</h3>
+                )}
+            </React.Fragment>
+        );
+    }
+    fetchUsers() {
+        fetch(`https://jsonplaceholder.typicode.com/users`)
+        .then(data =>
+            this.setState({
+                users: data,
+                isLoading: false,
+            })
+            )
+            .catch(error => this.setState({error, isLoading: false})
+            );
+    }
+    componentDidMount() {
+        this.fetchUsers();
+    }
+}
+
+// =======
+
+ReactDOM.render(
+    <Testap />,
+    document.getElementById('root')
+);
